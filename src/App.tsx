04a8b60754051793c91d9fb45d9c9263e96ac501
sham <shamlooo@yahoo.com>
2022-01-03 21:28:58 +0300

    Main files

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import Layout from './layout/Layout';
+import Wrapper from './utils/Hoc/Wrappers/Wrapper';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// *********************************
+
+class App extends Component {
+    // ----------------------------------------------------------------
+    render() {
+        return (
+            <Layout>
+                <Wrapper>Main</Wrapper>
+            </Layout>
+        );
+    }
 }
 
 export default App;
