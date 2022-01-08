ce93def1d39477a6e6813fc8ffd49a8df4d9e8c5
sham <shamlooo@yahoo.com>
2022-01-08 23:19:20 +0300

    ProductTypes

---
+++ src/store/types.ts
@@ -0,0 +1,31 @@
+type Attribute = {
+    displayValue: string;
+    value: string;
+    id: string;
+};
+type Currency = {
+    label: string;
+    symbol: string;
+};
+type Price = {
+    currency: Currency;
+    amount: number;
+};
+type Attributes = {
+    id: string;
+    name: string;
+    type: string;
+    items: [Attribute];
+};
+
+export type ProductType = {
+    id: string;
+    name: string;
+    inStock: boolean;
+    gallery: [string];
+    description: string;
+    category: string;
+    attributes: [Attributes];
+    prices: [Price];
+    brand: string;
+};
