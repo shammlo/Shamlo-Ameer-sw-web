a1b4df98d3897ebc4dfd883e041fd8f6febcf824
sham <shamlooo@yahoo.com>
2022-01-13 20:06:28 +0300

    Icon component

--- src/components/icon/Icon.tsx
+++ src/components/icon/Icon.tsx
@@ -118,6 +118,25 @@ class Icon extends Component<IconProps, IconState> {
                     </svg>
                 );
                 break;
+            case 'arrow-up':
+                icon = (
+                    <svg
+                        width="8"
+                        height="4"
+                        viewBox="0 0 8 4"
+                        fill="none"
+                        xmlns="http://www.w3.org/2000/svg"
+                        className={iconClass.join(' ')}
+                    >
+                        <path
+                            d="M1 3.5L4 0.5L7 3.5"
+                            stroke="black"
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                        />
+                    </svg>
+                );
+                break;
             case 'buy':
                 icon = (
                     <svg
