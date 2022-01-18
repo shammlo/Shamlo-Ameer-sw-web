66d90e7af5ca3784fe7edcc40724978552fea8a7
sham <shamlooo@yahoo.com>
2022-01-18 23:15:38 +0300

    global store, dev only

--- src/store/Store.ts
+++ src/store/Store.ts
@@ -2,18 +2,24 @@
 import { createStore, applyMiddleware, compose } from 'redux';
 import createSagaMiddleware from 'redux-saga';
 import { rootReducer } from './reducers/rootReducer';
-import logger from 'redux-logger';
+// import logger from 'redux-logger';
 import { productsSaga } from './sagas/rootSaga';
 //********************************
 
 const sagaMiddleware = createSagaMiddleware();
+const middleware = [sagaMiddleware];
+
+if (process.env.NODE_ENV === 'development') {
+    const { createLogger } = require('redux-logger');
+    middleware.push(createLogger());
+}
 
 const composeEnhancers =
     process.env.NODE_ENV === 'development'
         ? (typeof window != 'undefined' && (window as any)).__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
         : null || compose;
 
-const Store = createStore(rootReducer, composeEnhancers(applyMiddleware(sagaMiddleware, logger)));
+const Store = createStore(rootReducer, composeEnhancers(applyMiddleware(...middleware)));
 
 sagaMiddleware.run(productsSaga);
 
