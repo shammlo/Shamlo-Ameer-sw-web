d34e46895ac50c4a492b6f8c5e2e5c5b5c5da9c2
sham <shamlooo@yahoo.com>
2022-01-24 20:58:50 +0300

    Added arrow left and right

--- src/components/icon/Icon.tsx
+++ src/components/icon/Icon.tsx
@@ -221,6 +221,44 @@ class Icon extends Component<IconProps, IconState> {
                         </g>
                     </svg>
                 ));
+            case 'arrow-right':
+                return (icon = (
+                    <svg
+                        width="8"
+                        height="14"
+                        viewBox="0 0 8 14"
+                        fill="none"
+                        xmlns="http://www.w3.org/2000/svg"
+                        className={iconClass.join(' ')}
+                    >
+                        <path
+                            d="M1 13L7 7L1 1"
+                            stroke="white"
+                            strokeWidth="2"
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                        />
+                    </svg>
+                ));
+            case 'arrow-left':
+                return (icon = (
+                    <svg
+                        width="8"
+                        height="14"
+                        viewBox="0 0 8 14"
+                        fill="none"
+                        xmlns="http://www.w3.org/2000/svg"
+                        className={iconClass.join(' ')}
+                    >
+                        <path
+                            d="M7 13L1 7L7 1"
+                            stroke="white"
+                            strokeWidth="2"
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                        />
+                    </svg>
+                ));
 
             default:
                 icon = null;
